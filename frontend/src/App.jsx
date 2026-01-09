bba893edbfbb2a471563bba28c853f0dcb305406
Your Name <you@example.com>
2026-01-09 10:53:41 +0530

    authsuccess file added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Landing from "./pages/Landing";
 import Patterns from "./pages/Patterns";
 import Trends from "./pages/Trends";
 import Reflection from "./pages/Reflection";
-
+import AuthSuccess from "./pages/AuthSuccess";
 function App() {
   return (
     <BrowserRouter>
@@ -14,6 +14,8 @@ function App() {
         <Route path="/patterns" element={<Patterns />} />
         <Route path="/trends" element={<Trends />} />
         <Route path="/reflection" element={<Reflection />} />
+        <Route path="/auth-success" element={<AuthSuccess />} />
+
       </Routes>
     </BrowserRouter>
   );
